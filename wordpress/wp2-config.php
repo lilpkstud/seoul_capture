7ade7c12a5501a31c243f23650216dbbfb6b83a3
lilpkstud <paul.s.kwon92@gmail.com>
2024-02-05 21:23:47 -0800

    Testing 3

---
+++ wordpress/wp2-config.php
@@ -0,0 +1,13 @@
+<?php
+if(isset($_ENV[`JAWSDB_URL`])) {
+    $db = parse_url($_ENV[`JAWSDB_URL`]);
+    define("DB_NAME", trim($db[`path`],`/`));
+    define("DB_USER", $db[`user`]);
+    define("DB_PASSWORD", $db[`pass`]);
+    define("DB_HOST", $db[`host`]);
+    define("DB_CHARSET", ‘utf8’);
+    define("DB_COLLATE", ‘’);
+} else {
+	var_dump($db);
+    die("No Database credentials!");
+}
