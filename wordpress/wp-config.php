cce66a6ba6082fcbf7ed66edc30a7893cf763263
lilpkstud <paul.s.kwon92@gmail.com>
2024-02-09 16:31:11 -0800

    Update wp-config.php
    Cleaning-up Dotenv and pushing through for next steps

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -1,18 +1,4 @@
 <?php
-//use Dotenv\Dotenv;
-//$test = dirname(__FILE__, 2);
-//require $test.'/vendor/autoload.php';
-
-//$dotenv = Dotenv::createImmutable(__DIR__);
-//$dotenv->load();
-
-//$test = dirname(__FILE__, 2);
-//var_dump(dirname(__DIR__));
-//var_dump($test);
-//die();
-//$dotenv = Dotenv\Dotenv::createImmutable($test);
-//$dotenv->safeLoad();
-//$test = "hi";
 $url = getenv('JAWSDB_URL');
 $dbparts = parse_url($url);
 
@@ -28,5 +14,5 @@ $conn = mysqli_connect($hostname, $username, $password, $database);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connection was successfully established!";
+//echo "Connection was successfully established!";
 
