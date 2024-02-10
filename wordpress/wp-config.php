e78f98a56281040df7cdf1f2f553cb12e1bdb88b
lilpkstud <paul.s.kwon92@gmail.com>
2024-02-09 16:38:54 -0800

    Pushing rest to sync
    Pushh

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -14,5 +14,5 @@ $conn = mysqli_connect($hostname, $username, $password, $database);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-//echo "Connection was successfully established!";
+echo "Connection was successfully established!";
 
